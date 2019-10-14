3c809d992456297d306deee4aed8abd73bd6b163
auxo-chrome <52539367+auxo-chrome@users.noreply.github.com>
2019-10-14 14:35:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 /**
  * Track the trade of a commodity from one trader to another
- * @param {org.example.basic.mynetwork.Trade} trade - the trade to be
- * processed
- * @transaction trade
+ * @param {org.example.mynetwork.Trade} trade - the trade to be processed
+ * @transaction
  */
 
 async function tradeCommodity(trade) {
-	trade.commodity.owner = trade.newOwner;
-  	let assetRegistry = await getAssetRegistry("org.example.basic.mynetwork.Trade");
-  	await assetRegistry.update(trade.Commodity);
+    trade.commodity.owner = trade.newOwner;
+    let assetRegistry = await getAssetRegistry('org.example.mynetwork.Commodity');
+    await assetRegistry.update(trade.commodity);
 }
